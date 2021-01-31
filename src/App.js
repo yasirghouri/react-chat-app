f15d6e247b0b122e566ec02f8eed14078d638e56
yasirghouri <yasir.ag7@gmail.com>
2021-01-31 18:22:02 +0500

    added Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { ChatEngine } from "react-chat-engine";
 import "./App.css";
+import ChatFeed from "./components/ChatFeed/ChatFeed";
 
-function App() {
+const App = () => {
   return (
-    <div className="app">
-      <h1>Hello</h1>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID={process.env.REACT_APP_PROJECT_ID}
+      userName="yasirghouri"
+      userSecret="123321"
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
   );
-}
+};
 
 export default App;
---
+++ src/components/TheirMessage/TheirMessage.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const TheirMessage = ({ lastMessage, message }) => {
+  const isFirstMessageByUser =
+    !lastMessage || lastMessage.sender.username !== message.sender.username;
+  return (
+    <div className="message-row">
+      {isFirstMessageByUser && (
+        <div
+          className="message-avatar"
+          style={{ backgroundImage: `url(${message?.sender?.avatar})` }}
+        />
+      )}
+      {message?.attachments?.length > 0 ? (
+        <img
+          src={message.attachments[0].file}
+          alt="message-attachment"
+          className="message-image"
+          style={{ float: "right" }}
+        />
+      ) : null}
+      }
+    </div>
+  );
+};
+
+export default TheirMessage;
